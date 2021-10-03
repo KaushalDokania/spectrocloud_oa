0d612c568aad2185ca9450d85e204936bea1f0fc
Kaushal Dokania <kaushal.dokania@microfocus.com>
2021-10-03 16:38:31 +0530

    Fixed timeout issue

--- main.go
+++ main.go
@@ -29,20 +29,29 @@ type Task struct {
 	Status      string //completed, failed, timeout
 }
 
+type MyTask struct {
+	// started int64
+	started time.Time
+	task    *Task
+}
+
 const statusCompleted string = "completed"
 const statusFailed string = "failed"
 const statusTimeout string = "timeout"
 
 func main() {
 	fmt.Println("Hello World!!")
-	q := make(chan *Task, 10)
 
 	tasks := []Task{
-		{Id: "101", IsCompleted: false, Status: statusCompleted},
-		{Id: "102", IsCompleted: false, Status: statusCompleted},
-		{Id: "103", IsCompleted: false, Status: statusCompleted},
-		{Id: "104", IsCompleted: false, Status: statusCompleted},
-		{Id: "105", IsCompleted: false, Status: statusCompleted},
+		{Id: "101", IsCompleted: false, Status: statusFailed},
+		{Id: "102", IsCompleted: false, Status: statusFailed},
+		{Id: "103", IsCompleted: false, Status: statusFailed},
+		{Id: "104", IsCompleted: false, Status: statusFailed},
+		{Id: "105", IsCompleted: false, Status: statusFailed},
+		{Id: "106", IsCompleted: false, Status: statusFailed},
+		{Id: "107", IsCompleted: false, Status: statusFailed},
+		{Id: "108", IsCompleted: false, Status: statusFailed},
+		{Id: "109", IsCompleted: false, Status: statusFailed},
 	}
 
 	go func(tasks []Task) {
@@ -59,23 +68,38 @@ func main() {
 		}
 	}(tasks)
 
+	// q := make(chan *Task, 10)
+	q := make(chan *MyTask, 10)
+
 	// submitting all tasks to queue
 	for index, _ := range tasks {
 		fmt.Println("main: Adding task:", tasks[index].Id, " to queue")
-		q <- &tasks[index]
+		// q <- &tasks[index]
+		// time.Now().Unix()
+		// q <- &MyTask{started: time.Now().UnixNano() / int64(time.Millisecond), task: &tasks[index]}
+		q <- &MyTask{started: time.Now(), task: &tasks[index]}
 	}
 
 	count := 0
 	var list []string
 
 	for len(q) > 0 {
-		t := <-q
+		mt := <-q
+		t := mt.task
 		if t.IsCompleted {
+			t.Status = statusCompleted
 			list = append(list, t.Id)
 			fmt.Printf("\ntask: %s is [completed](Tasks completed: %v), len(queue): %d", t.Id, list, len(q))
 			count++
 		} else {
-			q <- t // push back if not completed
+			now := time.Now()
+			if now.Sub(mt.started) > time.Duration(5000*time.Millisecond) {
+				// if now-mt.started > 5000 {
+				fmt.Printf("discarding id: %s", t.Id)
+				t.Status = statusTimeout
+				continue
+			}
+			q <- mt // push back if not completed
 			fmt.Printf("\ntask: %s is NOT completed, pushing back to queue...(Tasks completed: %v), len(queue): %d", t.Id, list, len(q))
 		}
 		fmt.Println()
@@ -83,5 +107,6 @@ func main() {
 		time.Sleep(500 * time.Millisecond)
 	}
 
+	fmt.Printf("\n%v", tasks)
 	fmt.Println("\nThank you!!")
 }
