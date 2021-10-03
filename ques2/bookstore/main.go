6a0c4a6c1622a1f2f1e63532a16254e81e5b639b
Kaushal Dokania <kaushal.dokania@microfocus.com>
2021-10-03 20:17:02 +0530

    Added cli flags for reading database engine name

--- ques2/bookstore/main.go
+++ ques2/bookstore/main.go
@@ -2,25 +2,48 @@ package main
 
 import (
 	"bookstore/config"
+	"bookstore/models"
+	"flag"
 	"log"
 )
 
+var dbEngine string
+
+func init() {
+	// default database is mysql
+	databasePtr := flag.String("database", "mysql", "database for persistent storage")
+	flag.Parse()
+	dbEngine = *databasePtr
+}
+
+func configureDatabase(dbEngine string) {
+	if dbEngine == "mysql" {
+		err := config.ConfigureMySql()
+		if err != nil {
+			log.Printf("Error occured while setting up MySQL connection %s", err.Error())
+			panic(err.Error())
+		}
+		db := config.GetMysqlClient()
+		db.AutoMigrate(&models.Book{})
+		log.Println("connected to mysql successfully")
+	} else if dbEngine == "mongodb" {
+		err := config.ConfigureMongoDB()
+		if err != nil {
+			log.Printf("Error occured while setting up mongodb connection %s", err.Error())
+			panic(err.Error())
+		}
+		log.Println("connected to mongodb successfully")
+	} else {
+		log.Fatal("ERROR: Unknown database engine")
+	}
+}
+
+/*
+	--- commands to run ---
+	go run main.go
+	go run main.go -database mongodb
+*/
 func main() {
 	log.Println("Hello World !!")
-
-	// err := config.ConfigureMySql()
-	// if err != nil {
-	// 	log.Printf("Error occured while setting up MySQL connection %s", err.Error())
-	// 	panic(err.Error())
-	// }
-	// db := config.GetMysqlClient()
-	// db.AutoMigrate(&models.Book{})
-	// log.Println("connected to mysql successfully")
-
-	err := config.ConfigureMongoDB()
-	if err != nil {
-		log.Printf("Error occured while setting up mongodb connection %s", err.Error())
-		panic(err.Error())
-	}
-	log.Println("connected to mongodb successfully")
+	configureDatabase(dbEngine)
 }
